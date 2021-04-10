9fcdc10a780b42dc9a26ca002c2f20057d9f8028
Luis Rovirosa Mairlot <luis@codium.team>
2021-04-10 19:31:06 +0200

    AcceptanceTest the_example_provided_works_as_expected incomplete

---
+++ tests/AcceptanceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BagKata\Test;
+
+use BagKata\Bag;
+use BagKata\BagKata;
+use BagKata\FullBackException;
+use PHPUnit\Framework\TestCase;
+
+class AcceptanceTest extends TestCase
+{
+    /** @test */
+    public function the_example_provided_works_as_expected(): void
+    {
+        $this->markTestIncomplete('Not yet');
+        $bag = new Bag('Metals');
+        $bagKata = new BagKata($bag);
+        $bagKata->add('Leather');
+        $bagKata->add('Iron');
+        $bagKata->add('Copper');
+        $bagKata->add('Marigold');
+        $bagKata->add('Wool');
+        $bagKata->add('Gold');
+        $bagKata->add('Silk');
+        $bagKata->add('Copper');
+        $bagKata->add('Copper');
+        $bagKata->add('Cherry Blossom');
+
+        $bagKata->organize();
+
+        self::assertEquals(['Cherry Blossom', 'Iron', 'Leather', 'Marigold', 'Silk', 'Wool'], $bagKata->backpackItems());
+        self::assertEquals(['Copper', 'Copper', 'Copper', 'Gold'], $bag->items());
+    }
+}
